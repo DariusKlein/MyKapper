7333aca9b3eec42dec3c5164ccb3bfdeac373ac4
DariusKlein <54933874+DariusKlein@users.noreply.github.com>
2019-11-28 12:51:29 +0100

    coordinaten zijn van verschillende coordinaten systemen en matchen daarom niet (resultaat is afstand van 88 000 000 m of 88 000 km (aarde is bijna 7000 km))

--- app/src/main/java/com/example/mykapper/Loading.java
+++ app/src/main/java/com/example/mykapper/Loading.java
@@ -2,40 +2,31 @@ package com.example.mykapper;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.location.Location;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.widget.TextView;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.List;
-import java.util.Map;
-
-import static java.lang.String.valueOf;
-
 
 public class Loading extends AppCompatActivity {
 
     private TextView loading;
     private String DocID;
     private double Rating;
-    private GeoPoint Location;
-    private double lat1;
-    private double lat2;
-    private double lon1;
-    private double lon2;
+    private static GeoPoint Geolocation;
+    private static double lat2;
+    private static double lon2;
+    private Location loc1 = new Location("");
+    private Location loc2 = new Location("");
+
+
 
     private FusedLocationProviderClient fusedLocationClient;
 
@@ -57,9 +48,7 @@ public class Loading extends AppCompatActivity {
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
-                        //todo use location
-                        //todo FIX this lat2 = GeoPoint.getLatitude();
-                        //todo FiX this lon2 = GeoPoint.getLongitude ();
+                        loc1 = location;
                     }
                 });
 
@@ -74,26 +63,18 @@ public class Loading extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
 
 
-
-
-                                Location = document.getGeoPoint("Afstand");
+                                Geolocation = document.getGeoPoint("Afstand");
                                 Rating = document.getDouble("Rating");
                                 DocID = (document.getId());
 
+                                lat2 = Geolocation.getLatitude();
+                                lon2 = Geolocation.getLongitude();
 
-                                //todo FIX this lat1 = GeoPoint.getLatitude();
-                                //todo FIX this lon1 = GeoPoint.getLongitude ();
-
-                                Location loc1 = new Location("");
-                                loc1.setLatitude(lat1);
-                                loc1.setLongitude(lon1);
-
-                                Location loc2 = new Location("");
                                 loc2.setLatitude(lat2);
                                 loc2.setLongitude(lon2);
 
                                 float distanceInMeters = loc1.distanceTo(loc2);
-                                loading.setText(distanceInMeters + "Meter");
+                                loading.setText(distanceInMeters + " Meter");
 
                             }
                         }
